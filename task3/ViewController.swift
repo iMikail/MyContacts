c16416f67389aff35fbe989071dddf1af415a79c
Mikhail <imisha.volkov@gmail.com>
2022-12-19 16:33:44 +0300

    Initial Commit

---
+++ task3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task3
+//
+//  Created by Misha Volkov on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
