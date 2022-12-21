77af4e9c29c0c6303891df4a61b9ef66639a9ec9
Mikhail <imisha.volkov@gmail.com>
2022-12-21 18:38:28 +0300

    update(create) FavoritesContactsViewController

--- task3/Controller/FavoritesContactsViewController.swift
+++ task3/Controller/FavoritesContactsViewController.swift
@@ -8,9 +8,65 @@
 import UIKit
 
 final class FavoritesContactsViewController: UIViewController {
+  private let contactsManager = ContactsManager.shared
 
+  // MARK: - UIViews
+  private lazy var tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.register(ContactsTableViewCell.self, forCellReuseIdentifier: ContactsTableViewCell.identifier)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.dataSource = self
+    tableView.delegate = self
+
+    return tableView
+  }()
+
+  // MARK: - Functions
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .yellow
+    view.backgroundColor = .systemBackground
+    setupTableView()
   }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    tableView.reloadData()
+  }
+
+  private func setupTableView() {
+    view.addSubview(tableView)
+
+    NSLayoutConstraint.activate([
+      tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+      tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+    ])
+  }
+}
+
+// MARK: - Extension
+extension FavoritesContactsViewController: UITableViewDelegate, UITableViewDataSource {
+  internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return contactsManager.favoriteContacts.count
+  }
+
+  internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard
+      let contactCell = tableView.dequeueReusableCell(withIdentifier: ContactsTableViewCell.identifier,
+                                                 for: indexPath) as? ContactsTableViewCell
+    else {
+      return UITableViewCell()
+    }
+
+    contactCell.contact = contactsManager.favoriteContacts[indexPath.row]
+    contactCell.isUserInteractionEnabled = false
+
+    return contactCell
+  }
+
+  internal func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+  }
+
 }
